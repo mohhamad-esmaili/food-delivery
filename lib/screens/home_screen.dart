ca84bfc1b7d9847c0fbb037330efd82340493821
Mohammad Esmaili <esmaili.mohhamad@gmail.com>
2022-01-22 19:54:09 +0330

    adding search bar at top

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -33,8 +33,55 @@ class FoodHomeScreen extends StatelessWidget {
           TextButton(
             onPressed: () {},
             child: Text(
-              'Cart${currentUser.cart.length}',
-              style: TextStyle(color: Colors.white, fontSize: 18.0),
+              'Cart (${currentUser.cart.length})',
+              style: TextStyle(color: Colors.white, fontSize: 17.0),
+            ),
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                contentPadding: EdgeInsets.all(15.0),
+                prefixIcon: IconButton(
+                  icon: Icon(
+                    Icons.search,
+                  ),
+                  onPressed: () {},
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                    width: 0.8,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                    width: 0.8,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                    width: 0.8,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                hintText: 'Search Foods or Restaurant',
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.clear,
+                  ),
+                ),
+              ),
             ),
           )
         ],
